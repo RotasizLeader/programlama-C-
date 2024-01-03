e3c7bf7d569d4ba2d7c8f36b186fb6aeb3ee3755
Hasan Erkan Küçük <154514114+RotasizLeader@users.noreply.github.com>
2024-01-03 13:01:42 +0300

    Add files via upload

---
+++ programlama_Odev/programlama_Odev/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programlama_Odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Birinci Sayıyı giriniz: ");
+            double sayi1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("İkinici Sayıyı giriniz: ");
+            double sayi2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Girilen sayıların toplamı: ");
+            Console.WriteLine(sayi1 + sayi2);
+            Console.Write("Girilen sayıların farkı: ");
+            Console.WriteLine(sayi1 - sayi2);
+            Console.Write("Girilen sayıların çarpımı: ");
+            Console.WriteLine(sayi1 * sayi2);
+            Console.Write("Girilen sayıların bölümü: ");
+            Console.WriteLine(sayi1 / sayi2);
+            Console.ReadKey();
+        }
+    }
+}
